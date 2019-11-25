ef7fc5dde444659fb97e4f3ad572ac151372938d
Josh <jlreado@gmail.com>
2019-11-25 14:10:03 +0800

    inital commit

---
+++ assets/js/placeholder.js
@@ -0,0 +1,41 @@
+
+											
+
+
+						superplaceholder({
+							el: inp1,
+							sentences: ['M S Newaz', 'Ohidul Islam', 'Nazmul Hasan', 'Anika Mollik'],
+							options: {
+								letterDelay: 80,
+								loop: true,
+								startOnFocus: false
+							}
+						})
+					
+
+						superplaceholder({
+							el: inp2,
+							sentences: ['Only company emails', 'marveltheme@gmail.com', 'hasan.nazmulmmc@gamil.com'],
+							options: {
+								loop: true
+							}
+						})
+
+						superplaceholder({
+							el: inp3,
+							sentences: ['Password must 6 catecter long', '1 uppercase alphabet', 'Eg. hjhhjAsasd*'],
+							options: {
+								loop: true,
+								letterDelay: 30
+							}
+						})
+
+						superplaceholder({
+							el: inp4,
+							sentences: ['Any format works', 'http://yahoo.com', 'www.facebook.com', 'airbnb.com'],
+							options: {
+								letterDelay: 80,
+								loop: true,
+								startOnFocus: false
+							}
+						})
